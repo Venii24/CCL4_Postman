681dac3b2883bd022df1fa1bf4119a81e6b30784
Stefanie Plankensteiner <stefanieplankensteiner24@gmail.com>
2024-06-11 19:10:15 +0200

    added a transition from one scene to the next

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,49 +3,56 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
-
-public class GameManager : MonoBehaviour
+ 
+class GameManager : MonoBehaviour
 {
-    public static GameManager Instance {get; private set;}
+    public static GameManager Instance { get; private set; }
     private int _score = 0;
-    
+    private LevelLoader levelLoader;
+
     void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(this.gameObject); // This will make the GameManager persist between scenes
+            levelLoader = FindObjectOfType<LevelLoader>(); // Find the LevelLoader in the scene
         }
         else
         {
             Destroy(this.gameObject); // This will destroy the GameManager if another exists
         }
     }
-    
+
     public void LoadScene(string sceneName)
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        if (levelLoader != null)
+        {
+            levelLoader.LoadLevelByName(sceneName);
+        }
+        else
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        }
     }
-    
+
     public void AddScore(int score)
     {
         _score += score;
     }
-    
+
     public int GetScore()
     {
         return _score;
     }
-    
 
-    
     void Start()
     {
         
     }
-    
+
     void Update()
     {
-        Debug.Log("Score: " + _score);
+        //  Debug.Log("Score: " + _score);
     }
 }
