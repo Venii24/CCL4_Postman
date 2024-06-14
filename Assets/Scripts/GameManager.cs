98558f0f7d3bb5361861160eef0cb8d6599876eb
Stefanie Plankensteiner <stefanieplankensteiner24@gmail.com>
2024-06-14 14:37:48 +0200

    Fixed 2nd Dialogmanager when back to menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,31 +7,25 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    
     [Header("Overlays")]
     [SerializeField] public GameObject winOverlay;
     [SerializeField] public GameObject TimerBox;
     [SerializeField] public GameObject TimeOverOverlay;
     [SerializeField] public GameObject FKeyAlert;
-    
-     [Header("Dont Destroy On Load")]
-     [SerializeField] public GameObject DialogueManager;
-     [SerializeField] public GameObject Canvas;
-     
-     [Header("Buttons")]
-     [SerializeField] public TextMeshProUGUI ButtonLevelContinueText;
+
+    [Header("Dont Destroy On Load")]
+    [SerializeField] public GameObject DialogueManager;
+    [SerializeField] public GameObject Canvas;
+
+    [Header("Buttons")]
+    [SerializeField] public TextMeshProUGUI ButtonLevelContinueText;
     public static GameManager Instance { get; private set; }
     private Timer timer;
     private int score = 0;
     private LevelLoader levelLoader;
-    private bool BacktoMenu = false;
-   
-   
-   
+    private bool backToMenu = false;
 
-    
-
-    void Awake()
+    private void Awake()
     {
         timer = FindObjectOfType<Timer>();
         timer.stopTimer = true;
@@ -39,7 +33,7 @@ public class GameManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            if (!BacktoMenu)
+            if (!backToMenu)
             {
                 DontDestroyOnLoad(this.gameObject);
                 DontDestroyOnLoad(Canvas);
@@ -55,13 +49,13 @@ public class GameManager : MonoBehaviour
             }
             else
             {
-                Destroy(this.gameObject); // This will destroy the GameManager if another exists
+                Destroy(this.gameObject);
             }
-            
-
         }
-
-
+        else
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     private void Update()
@@ -72,25 +66,23 @@ public class GameManager : MonoBehaviour
             TimeOverOverlay.SetActive(false);
             FKeyAlert.SetActive(false);
             TimerBox.SetActive(false);
-            
         }
         else if (SceneManager.GetActiveScene().buildIndex == 1)
         {
             TimerBox.SetActive(true);
         }
-       
     }
 
     public void LoadScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         int nextSceneIndex = currentSceneIndex + 1;
-        
-        //Menu = 0 (By Built Index)
-        //Forest = 1
-        //Desert = 2
-        //Coast = 3
-        
+
+        // Menu = 0 (By Built Index)
+        // Forest = 1
+        // Desert = 2
+        // Coast = 3
+
         if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
         {
             Debug.Log("Loading next scene: " + nextSceneIndex);
@@ -100,15 +92,10 @@ public class GameManager : MonoBehaviour
             TimerBox.SetActive(true);
             timer.stopTimer = false;
         }
-        else if (nextSceneIndex == 4) //go back to menu
+        else if (nextSceneIndex == 4) // Go back to menu
         {
+            DestroyPersistentObjects();
             SceneManager.LoadScene(0);
-            winOverlay.SetActive(false);
-            TimeOverOverlay.SetActive(false);
-            TimerBox.SetActive(false);
-            timer.CountdownTime = 180f;
-            timer.stopTimer = true;
-            
         }
         else
         {
@@ -121,6 +108,19 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void DestroyPersistentObjects()
+    {
+        // Destroy GameManager instance
+        Destroy(this.gameObject);
+        
+        // Destroy other objects set to DontDestroyOnLoad
+        Destroy(Canvas);
+        Destroy(DialogueManager);
+
+        // Reset static instance
+        Instance = null;
+    }
+
     public void AddScore(int scoreToAdd)
     {
         score += scoreToAdd;
@@ -130,25 +130,25 @@ public class GameManager : MonoBehaviour
     {
         return score;
     }
-    
+
     public void ShowWinOverlay()
     {
         if (winOverlay != null)
             winOverlay.SetActive(true);
     }
-    
+
     public void ShowTimeOverOverlay()
     {
         if (TimeOverOverlay != null)
             TimeOverOverlay.SetActive(true);
     }
-    
+
     public void HideTimeOverOverlay()
     {
         if (TimeOverOverlay != null)
             TimeOverOverlay.SetActive(false);
     }
-    
+
     public void reloadScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
